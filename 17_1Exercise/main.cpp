05f7c78c6d6a54baccb213867dc6a577b07eb1a9
zhongta-cloud <59798331+zhongta-cloud@users.noreply.github.com>
2024-09-05 13:14:29 +0800

    Add files via upload

---
+++ 17_1Exercise/main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "input the string:" << endl;
+	char ch;
+	int count = 0;
+	while (cin.peek() != '#') {
+		cin.get(ch);
+		count++;
+	}
+
+	cout << "the count is " << count << endl;
+}
