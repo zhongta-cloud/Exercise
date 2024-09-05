8146fcb3113c09a0980945819e0d1bdb40e52a7a
zhongta-cloud <59798331+zhongta-cloud@users.noreply.github.com>
2024-09-05 13:22:49 +0800

    Add files via upload

---
+++ 17_2Exercise/main.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream fout;
+	fout.open("test.txt", ios_base::out);
+	cout << "input text:";
+	char buffer[255];
+	while (cin)
+	{
+		cin.getline(buffer, 255);
+		fout << buffer<<endl;
+	}
+
+	fout.close();
+}
